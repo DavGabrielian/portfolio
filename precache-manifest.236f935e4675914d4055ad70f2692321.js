2d298f2ebaf84a18f3dfdb087db1777ea39abdaa
David Gabrielian <davidgabrielofficial@gamil.com>
2020-06-29 20:42:03 +0400

    Updates

---
+++ precache-manifest.236f935e4675914d4055ad70f2692321.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4d28bd976586eb548dd4e6eeee20e4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c12e35b100cccf9347ca",
+    "url": "/portfolio/static/css/2.4f45e830.chunk.css"
+  },
+  {
+    "revision": "3922fc58a56773ff28aa",
+    "url": "/portfolio/static/css/main.c9726fa9.chunk.css"
+  },
+  {
+    "revision": "c12e35b100cccf9347ca",
+    "url": "/portfolio/static/js/2.9108ef99.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.9108ef99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3922fc58a56773ff28aa",
+    "url": "/portfolio/static/js/main.77d49ded.chunk.js"
+  },
+  {
+    "revision": "b417b3ee81e3e8f9aad5",
+    "url": "/portfolio/static/js/runtime-main.b649af6b.js"
+  },
+  {
+    "revision": "282f9be47276d0518e6a47bf9412530b",
+    "url": "/portfolio/static/media/CV.282f9be4.pdf"
+  },
+  {
+    "revision": "be440e2a518cbc3ca5c4c86e6ffd359b",
+    "url": "/portfolio/static/media/backgroundPic.be440e2a.jpg"
+  },
+  {
+    "revision": "ecb963581fe664a6c0daa963f73e8557",
+    "url": "/portfolio/static/media/coin.ecb96358.PNG"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "87afcb62c7d2b9de819c23479483298a",
+    "url": "/portfolio/static/media/logo.87afcb62.png"
+  },
+  {
+    "revision": "bbdeaeb28f5e11858d3a99c97532df52",
+    "url": "/portfolio/static/media/portfolio.bbdeaeb2.PNG"
+  },
+  {
+    "revision": "859dabbd7541075fe481c17b832a59d3",
+    "url": "/portfolio/static/media/profile0.859dabbd.png"
+  },
+  {
+    "revision": "2aa3410bfd5b3b5d049012c074843f91",
+    "url": "/portfolio/static/media/register.2aa3410b.PNG"
+  },
+  {
+    "revision": "a9a1e7e9157dbd606b68836f7ceda796",
+    "url": "/portfolio/static/media/star.a9a1e7e9.PNG"
+  },
+  {
+    "revision": "f009dcc0f062dfa110910fdfa42699ea",
+    "url": "/portfolio/static/media/todo.f009dcc0.PNG"
+  },
+  {
+    "revision": "90642eae9d50609735420a1ea90264e8",
+    "url": "/portfolio/static/media/website.90642eae.PNG"
+  }
+]);
